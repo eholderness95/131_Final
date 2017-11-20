247dab8364595cfddac4cb721656ef1058e95883
eholderness95 <egh@brandeis.edu>
2017-11-20 12:46:17 -0500

    post parser almost ready
    -- now accesses past 750 posts per page and writes to csv output
    -- completed pages are added to seperate csv, for debugging
    -- added exception for posts without text

--- get_access.py
+++ get_access.py
@@ -1,27 +1,33 @@
-import requests, json
+import requests
 import os, sys, csv
 
-ACCESS_TOKEN = '?access_token=EAAH1ZCjhNfBMBAFFvwhTRZAljgYCfrZBuPZCalNdauhyBFMfUEWAMiYMYIzhBdWZBE2GwJpECBMG0ICHZA9FZChGq3ZAlbXvy9RfLncZBF2K66LIH15hzOqzkJwJj4m4waCrKQKZAmdUW7GaqRefpSUbqFYgZCEFCyuCORgBmj1jOdFSaqJyJqklLQLxx3eejZBTFfYZD'
+ACCESS_TOKEN = '?access_token=551947191811091|aWKc4qfL6ZaUh8TQopALlxlN2Fs'
 URL = 'https://graph.facebook.com/'
 
-# def fetch_post_ids(self,n):
 
-with open(os.path.join(os.path.dirname(sys.argv[0]),'page_ids.csv')) as pages:
-    with open('post_ids.csv','w') as write_file:
-        reader = csv.reader(pages)
-        writer = csv.writer(write_file)
-        for row in reader:
-            if row[1] and row[1] != 'ID':
-                page_id = row[1]
-                posts = requests.get(URL + page_id + '/feed' + ACCESS_TOKEN)
-                cursor = 1
-                while cursor <= 30:
-                    data = posts.json()
-                    for post in data['data']:
-                        writer.writerow([el for el in post])
-                    cursor += 1
-                    posts = requests.get(data['paging']['next'])
-        write_file.close()
+with open(os.path.join(os.path.dirname(sys.argv[0]),'page_ids.csv')) as pages, open('post_ids.csv','w') as write_file:
+    reader = csv.reader(pages)
+    writer = csv.writer(write_file)
+    writer.writerow(['created_time','message','id'])
+    for row in reader:
+        if row[1] and row[1] != 'ID':
+            page_id = row[1]
+            posts = requests.get(URL + page_id + '/feed' + ACCESS_TOKEN)
+            cursor = 1
+            while cursor <= 30 and requests.get(data['paging']['next']):
+                data = posts.json()
+                for post in data['data']:
+                    try:
+                        writer.writerow([post['created_time'].encode('utf-8'),post['message'].encode('utf-8'),post['id'].encode('utf-8')])
+                    except Exception as inst:
+                        writer.writerow([post['created_time'].encode('utf-8'),'',post['id'].encode('utf-8')])
+                cursor += 1
+                posts = requests.get(data['paging']['next'])
+            with open('completed_pages.csv', 'w') as completed:
+                writer2 = csv.writer(completed)
+                writer2.writerow([row[1]])
+                completed.close()
+    write_file.close()
 
 
 # def fetch_comments(self,posts,page,n)
