479e39970b453c99c89f634baab82cf22135652e
eholderness95 <egh@brandeis.edu>
2017-12-12 13:48:48 -0500

    removed comments

--- create_directory.py
+++ create_directory.py
@@ -5,6 +5,7 @@ ACCESS_TOKENS = ['?access_token=551947191811091|aWKc4qfL6ZaUh8TQopALlxlN2Fs','?a
 URL = 'https://graph.facebook.com/'
 POST_FILE = open('post_ids.csv', 'r',encoding = "ISO-8859-1")
 REACTS = '&fields=reactions.type(LIKE).limit(0).summary(total_count).as(like),reactions.type(LOVE).limit(0).summary(total_count).as(love),reactions.type(HAHA).limit(0).summary(total_count).as(haha),reactions.type(WOW).limit(0).summary(total_count).as(wow),reactions.type(SAD).limit(0).summary(total_count).as(sad),reactions.type(ANGRY).limit(0).summary(total_count).as(angry)&limit=10'
+
 reader = csv.reader(POST_FILE)
 error_count = 0
 current_token = 0
@@ -17,6 +18,7 @@ def fetch_request(url):
         global error_count
         if 'error' in data and error_count < 10:
             print(data)
+            error_count += 1
             if data['error']['code'] == 4 or data['error']['code'] == 341:
                 print('Access limit reached for current token. Getting a new one')
                 global current_token
@@ -25,20 +27,24 @@ def fetch_request(url):
                 else:
                     print('Reached end of token list. Cycling back to top.')
                     current_token = 0
-            error_count += 1
+            elif data['error']['code'] == 100:
+                error_count -= 1
             print('\nRetrying....')
             return False
-        elif error_count >= 5:
+        elif error_count >= 10:
             print('Too many retries. Exiting.')
             sys.exit()
         return data
     except requests.exceptions.ConnectionError:
-        print("Connection refused")
+        print("Connection refused. Will retry after timeout.")
         from time import sleep
         sleep(5)
         return fetch_request(url)
-    except KeyError:
+    except KeyError or json.decoder.JSONDecodeError:
         return False
+
+
+
 
 
 def build_comments(comments, post_id):
@@ -65,15 +71,14 @@ def build_comments(comments, post_id):
                 json.dump(content, obj, indent=4, sort_keys=True)
         try:
             comments = fetch_request(comments['paging']['next'])
-        except KeyError:
+        except KeyError or False:
             break
-        except False:
-            break
+
 
 
 def parse_posts(reader):
     for post in reader:
-        if post[2] and post[2] != 'id':
+        if post[2] and post[2] != 'id' and post[0] and int(post[0][:4]) > 2015:
             global curr
             if curr != post[2].split('_')[0]:
                 curr = post[2].split('_')[0]
--- grab_posts.py
+++ grab_posts.py
@@ -1,40 +1,72 @@
-import requests
-import os, sys, csv, re, create_directory
+import os, sys, csv, re, requests
 
 ACCESS_TOKEN = '?access_token=551947191811091|aWKc4qfL6ZaUh8TQopALlxlN2Fs'
 URL = 'https://graph.facebook.com/'
-reader, writer, pages, write_file
+error_count = 0
 
+def fetch_request(url):
+    try:
+        data = requests.get(url).json()
+        global error_count
+        if 'error' in data and error_count < 10:
+            print(data)
+            if data['error']['code'] == 4 or data['error']['code'] == 341:
+                print('Access limit reached for current token. Getting a new one')
+                global current_token
+                if current_token < len(ACCESS_TOKEN) - 1:
+                    current_token += 1
+                else:
+                    print('Reached end of token list. Cycling back to top.')
+                    current_token = 0
+            elif data['error']['code'] == 100:
+                print('ouch')
+                error_count = 0
+            error_count += 1
+            print('\nRetrying....')
+            return False
+        elif error_count >= 5:
+            print('Too many retries. Exiting.')
+            sys.exit()
+        return data
+    except requests.exceptions.ConnectionError:
+        print("Connection refused. Will retry after timeout.")
+        from time import sleep
+        sleep(5)
+        return fetch_request(url)
+    except KeyError:
+        return False
 
 def init_csv():
-    global reader, writer, pages, write_file
     pages = open(os.path.join(os.path.dirname(sys.argv[0]), 'page_ids.csv'))
     write_file = open('post_ids.csv', 'a')
     reader = csv.reader(pages)
     writer = csv.writer(write_file)
     writer.writerow(['created_time', 'message', 'id'])
+    return reader, writer, pages, write_file
 
 
-def fetch_posts(data):
-    while 'next' in data['paging'] and not re.match(r'\b2015-10', data['data'][0]['id']):
+
+def fetch_posts(data, writer):
+    while 'next' in data['paging'] and not re.match(r'^2016-01', data['data'][0]['id']):
         for post in data['data']:
             try:
                 writer.writerow(
                     [post['created_time'].encode('utf-8'), post['message'].encode('utf-8'), post['id'].encode('utf-8')])
             except Exception as inst:
                 writer.writerow([post['created_time'].encode('utf-8'), '', post['id'].encode('utf-8')])
-        posts = requests.get(data['paging']['next'])
-        data = posts.json()
+        data = fetch_request(data['paging']['next'])
+        if not data:
+            break
 
 
 def parse_pages():
-    init_csv()
+    reader, writer, pages, write_file = init_csv()
     for row in reader:
         if row[1] and row[1] != 'ID':
             page_id = row[1]
-            data = create_directory.fetch_request(URL + page_id + '/feed?limit=100' + ACCESS_TOKEN)
+            data = fetch_request(URL + page_id + '/posts?limit=100' + ACCESS_TOKEN)
             print(row[0])
-            fetch_posts(data)
+            fetch_posts(data, writer)
             with open('completed_pages.csv', 'a') as completed:
                 writer2 = csv.writer(completed)
                 writer2.writerow([row[1]])
