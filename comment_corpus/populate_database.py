fb1cec0ea3c9fdfe903abfbf02e6128b03bee471
eholderness95 <egh@brandeis.edu>
2017-12-15 18:10:04 -0500

    Modularization
    -- Added directory for all module scripts
    -- Made seperate helper file for functions called across scripts
    -- Changed the way the app remembers if its done a task already

--- comment_corpus/populate_database.py
+++ comment_corpus/populate_database.py
@@ -1,13 +1,11 @@
 from pymongo import MongoClient
-from platform import system
-import os, re, sys, csv, json, ctypes
+import os, json, ctypes, helpers, atexit
 
 client = MongoClient()
 db = client.comment_corpus
 bulk = db.comments.initialize_unordered_bulk_op()
 print(db.command("serverStatus"))
 count = 1
-
 root_dir = os.getcwd()+os.sep+'Comments'
 
 def is_hidden(filepath):
@@ -23,7 +21,7 @@ def has_hidden_attribute(filepath):
         result = False
     return result
 
-def write_record(filepath,fun):
+def write_record(filepath, fun):
     global count, bulk
     try:
         # Write a record
@@ -36,27 +34,29 @@ def write_record(filepath,fun):
                 fun(doc, doc, upsert=True)
     except Exception as e:
         print("error inserting document")
-    if count % 500 == 0:
+    if count % 800 == 0:
         bulk.execute()
         bulk = db.comments.initialize_unordered_bulk_op()
 
+def parse_dir(dir, fun):
+    for subdir in os.listdir(dir):
+        dir_path = dir + os.sep + subdir
+        subdir_path = dir_path + os.sep + subdir
+        if not is_hidden(subdir):
+            if subdir.endswith(".json"):
+                write_record(subdir_path, fun)
+            elif os.path.isdir(subdir_path):
+                parse_dir(subdir_path, db.comments.update)
+
 def populate_database():
-    visited = {}
     for directory in os.listdir(root_dir):
         dir_path = root_dir + os.sep + directory
-        if not is_hidden(directory) and os.path.isdir(dir_path) and directory not in visited:
+        if not is_hidden(directory) and os.path.isdir(dir_path) and directory not in helpers.visited:
             print('Working on ' + directory + '....')
-            for subdir in os.listdir(dir_path):
-                subdir_path = dir_path+os.sep+subdir
-                if not is_hidden(subdir):
-                    if subdir.endswith(".json"):
-                        write_record(subdir_path,db.posts.update)
-                    elif os.path.isdir(subdir_path):
-                        for comment in os.listdir(subdir_path):
-                            comm_path = subdir_path+os.sep+comment
-                            if not is_hidden(comment) and comment.endswith(".json"):
-                                write_record(comm_path,db.comments.update)
-        visited[directory] = True
+            parse_dir(dir_path, db.posts.update)
+        helpers.visited[directory] = True
+
+atexit.register(helpers.save_progress)
 
 
 populate_database()
